7f483b4dee7f2d2eb58702dfaa7bcca8631ebebe
Recep Çankaya <recepcnky@outlook.com>
2024-01-19 02:50:19 +0300

    refactor: refactor gradient button into the customs

--- components/auth/forgotPassword/ResetConfirmation.tsx
+++ components/auth/forgotPassword/ResetConfirmation.tsx
@@ -23,6 +23,7 @@ import Svg, {
 } from "react-native-svg";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import CustomGradientButton from "../../customs/CustomGradientButton";
 
 const ResetConfirmation: React.FC = () => {
   const [code, setCode] = useState("");
@@ -55,49 +56,7 @@ const ResetConfirmation: React.FC = () => {
       </View>
       <View style={styles.buttonContainer}>
         <TouchableOpacity onPress={handleSubmit}>
-          <Svg width="168" height="157" viewBox="0 0 177 157" fill="none">
-            <Path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M109.119 6.28364C125.645 9.54071 136.874 23.7587 151.609 32.0213C167.73 41.0608 191.703 40.3053 199.769 57.1128C207.807 73.8619 191.716 92.3733 189.443 110.861C187.111 129.843 196.896 151.2 186.264 166.995C175.554 182.907 154.52 188.842 135.635 190.807C118.409 192.598 103.331 181.459 86.4893 177.372C68.4574 172.996 47.9926 177.249 33.1719 165.941C17.2693 153.808 5.09061 134.639 4.04704 114.473C3.02277 94.6792 18.9714 79.1193 27.7632 61.4214C36.1565 44.526 39.0109 23.3912 54.5136 12.8845C70.0737 2.33886 90.7526 2.66378 109.119 6.28364Z"
-              fill="#050505"
-              stroke="url(#gradient)"
-              strokeWidth="7"
-            />
-            <Defs>
-              <SvgLinearGradient id="gradient" x1="0" y1="0" x2="1" y2="0">
-                <Stop offset="0%" stopColor="#B80DCA" />
-                <Stop offset="100%" stopColor="#4035CB" />
-              </SvgLinearGradient>
-            </Defs>
-          </Svg>
-          <MaskedView
-            style={styles.buttonText}
-            maskElement={
-              <Text
-                style={{
-                  fontSize: 26,
-                  fontFamily: "Inter",
-                  fontStyle: "italic",
-                }}>
-                Submit
-              </Text>
-            }>
-            <LinearGradient
-              colors={["#B80DCA", "#4035CB"]}
-              start={{ x: 0, y: 0 }}
-              end={{ x: 1, y: 0 }}>
-              <Text
-                style={{
-                  fontSize: 26,
-                  fontFamily: "Inter",
-                  fontStyle: "italic",
-                  opacity: 0,
-                }}>
-                Submit
-              </Text>
-            </LinearGradient>
-          </MaskedView>
+          <CustomGradientButton text="Confirm" />
         </TouchableOpacity>
       </View>
     </SafeAreaView>
@@ -137,16 +96,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     flex: 1,
-  },
-  buttonText: {
-    fontFamily: "Inter",
-    fontSize: 22,
-    fontStyle: "italic",
-    fontWeight: "400",
-    right: 30,
-    bottom: 40,
-    position: "absolute",
-    zIndex: 1,
   },
 });
 
