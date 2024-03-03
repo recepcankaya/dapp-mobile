f82cb71c2b55ab1dfbbaf1f1cbc5005a0473dac8
Recep Cankaya <recepcnky@outlook.com>
2024-03-04 00:47:30 +0300

    feat: finishing admin logic

--- src/screens/admin/AdminCamera.tsx
+++ src/screens/admin/AdminCamera.tsx
@@ -5,6 +5,7 @@ import {
   Text,
   ActivityIndicator,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 import {
   Camera,
@@ -49,58 +50,115 @@ const AdminCamera = () => {
   const codeScanner = useCodeScanner({
     codeTypes: ["qr", "ean-13"],
     onCodeScanned: async (codes) => {
-      const value = codes[0].value;
+      let userId = "";
+      let forNFT = null;
+
+      if (typeof codes[0].value === "string") {
+        const parsedValue = JSON.parse(codes[0].value);
+        ({ userId, forNFT } = parsedValue);
+      }
+
+      // get number_of_orders, has_free_right, number_of_free_rights from user_missions table
       const { data: userMissionInfo, error: errorUserMissionInfo } =
         await supabase
           .from("user_missions")
           .select("number_of_orders, has_free_right, number_of_free_rights")
-          .eq("user_id", value);
+          .eq("user_id", userId);
+
+      // get number_for_reward from admin table
       const { data: numberForReward, error: errorNumberForReward } =
         await supabase
           .from("admin")
           .select("number_for_reward")
           .eq("id", adminId);
 
-      if (!userMissionInfo) {
-        // !userMissionInfo = insert et
-        await supabase.from("user_missions").insert({
-          number_of_orders: 1,
-          user_id: value,
-          admin_id: adminId,
-          has_free_right: false,
-          number_of_free_rights: 0,
-        });
-      } else if (
-        numberForReward &&
-        userMissionInfo[0].number_of_orders <
-          numberForReward[0].number_for_reward
-      ) {
-        // userMissionInfo < numberForReward = 1 ekle
-        await supabase
-          .from("user_missions")
-          .update({ number_of_orders: userMissionInfo[0].number_of_orders + 1 })
-          .eq("user_id", value);
-      } else {
-        // userMissionInfo = numberForReward = bilgileri güncelle ve nft mintle
-        await supabase
-          .from("user_missions")
-          .update({
-            has_free_right: true,
-            number_of_free_rights: userMissionInfo[0].number_of_free_rights + 1,
-            number_of_orders: 0,
-          })
-          .eq("user_id", value);
-        // BURADA KULLANICIYA NFTSİNİ MİNTLEYELİM
-        if (address) {
-          mintNft({
-            metadata: {
-              name: "Mega Playstation",
-              description: "",
-              image:
-                "ipfs://QmP7NbUz8FhcBuRKC4NeXokkTWXZKtTRdPijM9yc2FPExU/Mega%20Playstation.png",
-            },
-            to: address,
+      // If the order is for free, check the user's free right
+      if (forNFT === true) {
+        // Check whether the user has a free right
+        if (userMissionInfo && userMissionInfo[0].has_free_right > 1) {
+          // If the user has more than one free right, decrease the number of free rights by one
+          await supabase
+            .from("user_missions")
+            .update({
+              number_of_free_rights:
+                userMissionInfo[0].number_of_free_rights - 1,
+            })
+            .eq("user_id", userId);
+          Alert.alert("Müşterinize ödülünüzü verebilirsiniz.");
+        } else if (userMissionInfo && userMissionInfo[0].has_free_right === 1) {
+          // If the user has only one free right, delete the row
+          await supabase.from("user_missions").delete().eq("id", userId);
+          Alert.alert("Müşterinize ödülünüzü verebilirsiniz.");
+        } else {
+          Alert.alert("Müşterinizin ücretsiz hakkı bulunmamaktadır.");
+        }
+      }
+      // If the order is not for free, check the number of orders
+      else {
+        if (!userMissionInfo) {
+          // If the user does not have a record in the user_missions table, add a new record
+          await supabase.from("user_missions").insert({
+            number_of_orders: 1,
+            user_id: userId,
+            admin_id: adminId,
+            has_free_right: false,
+            number_of_free_rights: 0,
           });
+        } else if (
+          numberForReward &&
+          userMissionInfo[0].number_of_orders <
+            numberForReward[0].number_for_reward
+        ) {
+          // If the user has a record in the user_missions table and the number of orders is less than the number_for_reward, increase the number of orders by one
+          await supabase
+            .from("user_missions")
+            .update({
+              number_of_orders: userMissionInfo[0].number_of_orders + 1,
+            })
+            .eq("user_id", userId);
+        } else {
+          // If the user has a record in the user_missions table and the number of orders is equal to the number_for_reward, mint the NFT and reset the number of orders
+          if (userMissionInfo[0].has_free_right === false) {
+            await supabase
+              .from("user_missions")
+              .update({
+                has_free_right: true,
+                number_of_free_rights: 1,
+                number_of_orders: 0,
+              })
+              .eq("user_id", userId);
+            if (address) {
+              mintNft({
+                metadata: {
+                  name: "Mega Playstation",
+                  description: "",
+                  image:
+                    "ipfs://QmP7NbUz8FhcBuRKC4NeXokkTWXZKtTRdPijM9yc2FPExU/Mega%20Playstation.png",
+                },
+                to: address,
+              });
+            }
+          } else {
+            await supabase
+              .from("user_missions")
+              .update({
+                number_of_free_rights:
+                  userMissionInfo[0].number_of_free_rights + 1,
+                number_of_orders: 0,
+              })
+              .eq("user_id", userId);
+            if (address) {
+              mintNft({
+                metadata: {
+                  name: "Mega Playstation",
+                  description: "",
+                  image:
+                    "ipfs://QmP7NbUz8FhcBuRKC4NeXokkTWXZKtTRdPijM9yc2FPExU/Mega%20Playstation.png",
+                },
+                to: address,
+              });
+            }
+          }
         }
       }
     },
