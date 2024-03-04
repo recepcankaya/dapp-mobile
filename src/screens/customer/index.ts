f96c5c3f11bebef3d30f7e37d27298c8b0d242ce
Recep Cankaya <recepcnky@outlook.com>
2024-03-04 21:50:17 +0300

    feat: adding profile component and resolve merge conflicts

--- src/screens/customer/index.ts
+++ src/screens/customer/index.ts
@@ -2,5 +2,6 @@ import Login from "./Login";
 import UserInfo from "./UserInfo";
 import Brands from "./Brands";
 import CustomerHome from "./CustomerHome";
+import Profile from "./Profile";
 
-export { Login, UserInfo, Brands, CustomerHome };
+export { Login, UserInfo, Brands, CustomerHome, Profile };
