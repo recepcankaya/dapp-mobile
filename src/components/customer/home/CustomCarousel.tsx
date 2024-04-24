9ac198c891c3e743ce9ffab61ecdb87c4906ce03
Atakan <42267756+atakanotur@users.noreply.github.com>
2024-04-24 14:21:49 +0300

    style changes

--- src/components/customer/home/CustomCarousel.tsx
+++ src/components/customer/home/CustomCarousel.tsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
-import { View, ScrollView, Pressable, Image, Dimensions, Modal, StyleSheet, FlatList } from 'react-native';
+import { View, Pressable, Image, Dimensions, Modal, StyleSheet, FlatList } from 'react-native';
 
 import colors from '../../../ui/colors';
 
 type CustomCarouselProps = {
     data: { image: string }[]
 }
+const { width, height } = Dimensions.get('window');
+const CARD_WIDTH = width;
 
 const CustomCarousel = ({ data }: CustomCarouselProps) => {
     const [selectedImage, setSelectedImage] = useState<string | null>(null);
     const [carouselItems, setCarouselItems] = useState(data);
-    const { width, height } = Dimensions.get('window');
-    const CARD_WIDTH = width;
 
     const openImage = (imageUri: string) => {
         setSelectedImage(imageUri);
@@ -22,7 +22,7 @@ const CustomCarousel = ({ data }: CustomCarouselProps) => {
     };
 
     return (
-        <View style={{ flex: 1, justifyContent: 'center', backgroundColor: 'white' }}>
+        <View style={styles.container}>
             <FlatList
                 data={carouselItems}
                 horizontal
@@ -31,8 +31,8 @@ const CustomCarousel = ({ data }: CustomCarouselProps) => {
                 snapToAlignment="center"
                 scrollEventThrottle={16}
                 decelerationRate={0}
-                contentContainerStyle={{ backgroundColor: colors.black, marginTop: 10 }}
-                style={{ backgroundColor: colors.black }}
+                style={styles.carousel}
+                contentContainerStyle={styles.carouselContentContainer}
                 pagingEnabled
                 onEndReachedThreshold={0.5}
                 onEndReached={() => setCarouselItems([...carouselItems, ...data])}
@@ -40,10 +40,10 @@ const CustomCarousel = ({ data }: CustomCarouselProps) => {
                     return (
                         <Pressable
                             key={index}
-                            style={{ width: CARD_WIDTH, alignSelf: 'center', alignItems: 'center' }}
+                            style={styles.carouselItem}
                             onPress={() => openImage(item.image)}
                         >
-                            <Image source={{ uri: item.image }} style={{ height: 200, width: CARD_WIDTH }} resizeMode='contain' />
+                            <Image source={{ uri: item.image }} style={styles.carouselItemImage} resizeMode='contain' />
                         </Pressable>
                     )
                 }}
@@ -51,7 +51,7 @@ const CustomCarousel = ({ data }: CustomCarouselProps) => {
             <Modal visible={!!selectedImage} transparent={true} onRequestClose={closeImage}>
                 <View style={styles.modalBackground}>
                     <Pressable style={styles.modalContent} onPress={closeImage}>
-                        <Image source={{ uri: selectedImage ? selectedImage : '' }} style={{ flex: 1 }} resizeMode='contain' />
+                        <Image source={{ uri: selectedImage ? selectedImage : '' }} style={styles.modalImage} resizeMode='contain' />
                     </Pressable>
                 </View>
             </Modal>
@@ -60,6 +60,26 @@ const CustomCarousel = ({ data }: CustomCarouselProps) => {
 };
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: colors.white
+    },
+    carousel: {
+        backgroundColor: colors.black
+    },
+    carouselContentContainer: {
+        backgroundColor: colors.black,
+        marginTop: 10
+    },
+    carouselItem: {
+        width: CARD_WIDTH,
+        alignItems: 'center'
+    },
+    carouselItemImage: {
+        height: 180,
+        width: CARD_WIDTH
+    },
     modalBackground: {
         flex: 1,
         backgroundColor: 'rgba(0, 0, 0, 0.9)',
@@ -70,6 +90,9 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
     },
+    modalImage: {
+        flex: 1
+    }
 });
 
 export default CustomCarousel;
